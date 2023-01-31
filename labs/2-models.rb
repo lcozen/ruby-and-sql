ef228368164fc6e618febb2536f42412884e4040
lcozen <lindsay.cozen@kellogg.northwestern.edu>
2023-01-31 21:58:59 +0000

    changes

--- labs/2-models.rb
+++ labs/2-models.rb
@@ -21,12 +21,41 @@ Salesperson.destroy_all
 
 # 2. insert 1-2 rows in salespeople table.
 
+#new_company = Company.new
+#new_company ["name"] = "Apple"
+#new_company ["city"] = "Cupertino"
+#new_company ["state"] = "CA"
+
+salesperson = Salesperson.new 
+salesperson ["first_name"] = "Ben"
+salesperson ["last_name"] = "Block"
+salesperson ["email"] = "ben@test.com"
+
+salesperson.save
+
+salesperson = Salesperson.new
+salesperson ["first_name"] = "Brian"
+salesperson ["last_name"] = "Eng"
+salesperson ["email"] = "brian@test.com"
+
+salesperson.save
+
+
 # 3. write code to display how many salespeople rows are in the database
+
+puts "Salespeople: #{Salesperson.all.count}"
 
 # ---------------------------------
 # Salespeople: 2
 
 # 4. modify/update column data for a row in the salespeople table.
+
+ben = Salesperson.find_by ({"first_name" => "Ben", "last_name" => "Block"})
+#puts ben.inspect
+
+ben["email"] = "benjamin.block@kellogg.northwestern.edu"
+ben.save
+#puts ben.inspect
 
 # CHALLENGE:
 # 5. write code to display each salesperson's full name
@@ -35,3 +64,12 @@ Salesperson.destroy_all
 # Salespeople: 2
 # Ben Block
 # Brian Eng
+
+all_salespeople = Salesperson.all
+for person in all_salespeople
+
+    first_name = person["first_name"]
+    last_name = person["last_name"]
+
+puts "#{first_name} #{last_name}"
+end
